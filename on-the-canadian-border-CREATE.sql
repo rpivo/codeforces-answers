9eea4a27462ba4f9bd6e7bcbfc45a47cb4e60c01
Ryan Pivovar <ryanpivovar@gmail.com>
2019-10-27 15:33:19 -0400

    move files

--- on-the-canadian-border-CREATE.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE travelers (name VARCHAR (50) NOT NULL,
-                                          country VARCHAR (50) NOT NULL);
--- on-the-canadian-border-SELECT.sql
+++
@@ -1,6 +0,0 @@
-SELECT name,
-       country
-FROM public.travelers
-WHERE country NOT IN ('USA',
-                      'Canada',
-                      'Mexico');
