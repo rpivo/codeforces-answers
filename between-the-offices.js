534c1a5999bc6c619bf4c7edc13c8215dd349fdc
Ryan Pivovar <ryanpivovar@gmail.com>
2019-10-27 19:33:32 -0400

    update between-the-offices

--- between-the-offices.js
+++ between-the-offices.js
@@ -9,9 +9,8 @@ let days, toSF = toSea = 0;
 
 rl.on('line', n => {
   if (!days) { days = n } else {
-    n.toLowerCase().split('')
-      .forEach((letter, i, arr) =>
-          (letter !== arr[i - 1] && i !== 0) && (letter === 'f' ? toSF++ : toSea++));
+    n.toLowerCase().split('').forEach((letter, i, arr) =>
+      (letter !== arr[i - 1] && i !== 0) && (letter === 'f' ? toSF++ : toSea++));
     console.log(toSF > toSea ? 'YES' : 'NO');
     rl.close();
   }
