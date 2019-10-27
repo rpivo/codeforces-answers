5fa76becfd92d126732e15f40ea50532d8fd35f9
Ryan Pivovar <ryanpivovar@gmail.com>
2019-10-27 19:03:06 -0400

    solve between-the-offices

---
+++ between-the-offices.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let days, toSF = toSea = 0;
+
+rl.on('line', n => {
+  if (!days) { days = n } else {
+    n.toLowerCase().split('')
+      .forEach((letter, i, arr) =>
+          letter !== arr[i - 1] && i !== 0 && (letter === 'f' ? toSF++ : toSea++));
+    console.log(toSF > toSea ? 'YES' : 'NO');
+    rl.close();
+  }
+});
